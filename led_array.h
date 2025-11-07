3be7cfec2900e8a8eb16c8ee3c7417c983ed88cc
Andrew Poon <poonan@oregonstate.edu>
2025-11-07 04:39:22 -0800

    create header

---
+++ led_array.h
@@ -0,0 +1,11 @@
+/*
+File: led_array.h
+Language: C
+Author: Andrew Poon
+Date: 11/07/25
+Description: Provides the public interface for controlling the WS2812
+    RGB LED array. Declares constants and functions for initializing
+    LED array, mapping humidity values to LED levels, and displaying
+    loading or error animations.
+*/
+
