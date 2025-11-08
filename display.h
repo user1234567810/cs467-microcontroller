462378893fce57916008ced83333e1b830ff3727
Andrew Poon <poonan@oregonstate.edu>
2025-11-08 02:57:48 -0800

    created header

---
+++ display.h
@@ -0,0 +1,10 @@
+/*
+File: display.h
+Language: C
+Author: Andrew Poon
+Date: 11/08/25
+Description: Provides the public interface for controlling the LCD1602
+    display. Declares constants and functions for initializing the
+    LCD display, displaying humidity values to LCD, and displaying
+    system status / error visualizations.
+*/
